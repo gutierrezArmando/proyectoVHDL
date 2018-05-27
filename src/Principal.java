2b746b0a6cf54fa2441407fb16244e6c9b83dc2c
Armando Gutiérrez <gutierrezjarr@gmail.com>
2018-05-27 15:51:10 -0800

    prueba IntelliJ IDEA

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,26 @@
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Proyecto VHDL");
+        Persona p = new Persona("Armando", 34);
+        System.out.println(p.toString());
+    }
+}
+
+class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Persona() {
+        this("", 0);
+    }
+
+    @Override
+    public String toString () {
+        return String.format("Nombre: %s\nEdad: %d", this.nombre, this.edad);
     }
 }
